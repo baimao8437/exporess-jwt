11d1e5a444cf0a4b897b2d973ccbbb189a37a7b6
baimao shih <baimao@xfers.com>
2021-05-29 18:02:17 +0800

    add body-parser

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express')
+const bp = require('body-parser')
+
 const app = express()
+const port = 3000
+
+// we don't have db so just use this to store user
+const users = []
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-app.listen(3000)
+app.post('/register', (req, res) => {
+  console.log(req.body)
+  res.send('success')
+})
+
+app.listen(port, () => {
+  console.log(`Express server listening at http://localhost:${port}`)
+})
